4e3e0b9209b53907fcd9863cced7f16358528801
n_matsuo <n.matsuo1616@gmail.com>
2022-03-23 18:21:00 +0900

    add grid and title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,22 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { NextUIProvider } from '@nextui-org/react'
+import { DefaultSeo } from 'next-seo'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <NextUIProvider>
+      <DefaultSeo
+        title='おつまみ.dev'
+        twitter={{}}
+        openGraph={{
+          title: 'おつまみ（まつお）のページ',
+          url: 'https://otsumami.dev',
+          type: 'website',
+          description: 'おつまみ（まつお）のTOPページ',
+          site_name: 'otsumami.dev',
+        }}
+      />
       <Component {...pageProps} />)
     </NextUIProvider>
   )
