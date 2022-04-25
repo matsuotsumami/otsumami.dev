407898bf38c93d9d4e7a564452feb87573bcd44a
n_matsuo <n.matsuo1616@gmail.com>
2022-04-25 17:48:14 +0900

    nextjs reset

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { NextUIProvider } from '@nextui-org/react'
-import { DefaultSeo } from 'next-seo'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <NextUIProvider>
-      <DefaultSeo
-        title='おつまみ.dev'
-        twitter={{}}
-        openGraph={{
-          title: 'おつまみ（まつお）のページ',
-          url: 'https://otsumami.dev',
-          type: 'website',
-          description: 'おつまみ（まつお）のTOPページ',
-          site_name: 'otsumami.dev',
-        }}
-      />
-      <Component {...pageProps} />)
-    </NextUIProvider>
-  )
+  return <Component {...pageProps} />
 }
 
 export default MyApp
